820b6832bf448e2378ced2ead5d39f39adc16de0
Eric Grove <odin36035@gmail.com>
2016-04-05 14:26:35 -0400

    Added files via upload

--- VisualInventory.js
+++ VisualInventory.js
@@ -1,8 +1,8 @@
 var w = window.innerWidth, h = window.innerHeight;
 
 var svg = d3.select("body").append("svg")
-	.attr("width", w*0.7)
-	.attr("height", h*0.7)
+	.attr("width", w)
+	.attr("height", h)
 	.style("background", "white");
 
 	var buildingColor = "orange";
@@ -41,8 +41,10 @@ var svg = d3.select("body").append("svg")
 		var roomDeviceMatrix = "matrix(0.2258128,0,0,0.2258128,531.70261,287.10811)";
 		var deviceMatrix = "matrix(1.157551,0,0,1.0770259,79.525472,254.61494)";
 
+	var svg2 = svg.append("g").attr("transform", "scale(0.7)");
+
 	// Campus Map
-	var CampusMap = svg.append("g")
+	var CampusMap = svg2.append("g")
 		.attr("id", "CampusMap")
 		.attr("transform", "matrix(1,0,0,1,-190,-137)");
 
@@ -516,7 +518,7 @@ var svg = d3.select("body").append("svg")
 				.attr("fill-opacity", 1);
 
 	// Stack of FLoors
-	var SFBTg = svg.append("g")
+	var SFBTg = svg2.append("g")
 		.attr("id", "SFBTg")
 		.attr("class", "SFg")
 		.attr("transform", SFgThumbMatrix);
@@ -586,7 +588,7 @@ var svg = d3.select("body").append("svg")
 			.attr("fill-opacity", 0);
 
 	// floors
-	var BT2g = svg.append("g")
+	var BT2g = svg2.append("g")
 		.attr("id", "BT2g")
 		.attr("class", "floorG")
 		.attr("transform", "matrix(0.16477916,0,0,0.16477916,230,0)");
@@ -648,7 +650,7 @@ var svg = d3.select("body").append("svg")
 			});
 
 	// rooms
-	var BT253g = svg.append("g")
+	var BT253g = svg2.append("g")
 		.attr("id", "BT253g")
 		.attr("class", "roomG")
 		.attr("transform", roomGthumbMatrix);
@@ -676,7 +678,7 @@ var svg = d3.select("body").append("svg")
 
 
 		var BT253device = BT253g.append("g")
-			.attr("id", "IBT0253D53684")
+			.attr("id", "IBT0253D56843")
 			.attr("transform", roomDeviceMatrix);
 
 			var BT253devicePath1 = BT253device.append("path")
@@ -692,20 +694,20 @@ var svg = d3.select("body").append("svg")
 				.attr("fill", "#999999")
 				.attr("fill-opacity", 0)
 				.on("click", function(){
-					d3.select("#IBT0253D53684g")
+					d3.select("#IBT0253D56843g")
 						.transition()
 						.duration(Dur)
 						.attr("transform", "matrix(1,0,0,1,18,7)")
-					d3.select("#IBT0253D53684g").selectAll(".background")
+					d3.select("#IBT0253D56843g").selectAll(".background")
 						.transition()
 						.duration(Dur)
 						.attr("fill-opacity", BGopacity)
-					d3.select("#IBT0253D53684g").selectAll("path")
+					d3.select("#IBT0253D56843g").selectAll("path")
 						.transition()
 						.duration(Dur)
 						.attr("fill-opacity", 1)
 						.attr("stroke-opacity", 1)
-					d3.select("#IBT0253D53684g").selectAll("text")
+					d3.select("#IBT0253D56843g").selectAll("text")
 						.transition()
 						.duration(Dur)
 						.attr("opacity", 1)
@@ -734,13 +736,13 @@ var svg = d3.select("body").append("svg")
 				.attr("stroke-width", 1);
 
 	// devices
-	var IBT0253D53684g = svg.append("g")
-		.attr("id", "IBT0253D53684g")
+	var IBT0253D56843g = svg2.append("g")
+		.attr("id", "IBT0253D56843g")
 		.attr("class", "deviceG")
 		.attr("transform", deviceGthumbMatrix);
 
-		var IBT0253D53684BG = IBT0253D53684g.append("rect")
-			.attr("id", "IBT0253D53684BG")
+		var IBT0253D56843BG = IBT0253D56843g.append("rect")
+			.attr("id", "IBT0253D56843BG")
 			.attr("class", "background")
 			.attr("width", deviceBG.width)
 			.attr("height", deviceBG.height)
@@ -750,8 +752,8 @@ var svg = d3.select("body").append("svg")
 			.attr("fill", "gray")
 			.attr("fill-opacity", 0);
 
-		var device = IBT0253D53684g.append("g")
-			.attr("id", "IBT0253D53684")
+		var device = IBT0253D56843g.append("g")
+			.attr("id", "IBT0253D56843")
 			.attr("transform", deviceMatrix);
 
 			var devicePath1 = device.append("path")
@@ -781,8 +783,8 @@ var svg = d3.select("body").append("svg")
 				.attr("stroke-opacity", 0)
 				.attr("stroke-width", 1);
 
-		var deviceName = IBT0253D53684g.append("text")
-				.text("I-BT0253-D53684")
+		var deviceName = IBT0253D56843g.append("text")
+				.text("I-BT0253-D56843")
 				.attr("class", "deviceInfo")
 				.attr("id", "deviceNameText")
 				.attr("pointer-events", "none")
@@ -793,7 +795,7 @@ var svg = d3.select("body").append("svg")
 				.attr("font-weight", "bold")
 				.attr("opacity", 0);
 
-		var deviceModelOS = IBT0253D53684g.append("text")
+		var deviceModelOS = IBT0253D56843g.append("text")
 				.text("7010 Windows 7")
 				.attr("class", "deviceInfo")
 				.attr("id", "deviceModelText")
@@ -805,7 +807,7 @@ var svg = d3.select("body").append("svg")
 				.attr("font-weight", "bold")
 				.attr("opacity", 0);
 
-		var issues = IBT0253D53684g.append("text")
+		var issues = IBT0253D56843g.append("text")
 				.text("Issues:")
 				.attr("class", "deviceInfo")
 				.attr("id", "deviceIssuesText")
